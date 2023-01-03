fba58ff63fefad14a5ef8aea8e0e9e3d5bbd1e3d
rcarabal2022 <raquelcaraballospain@gmail.com>
2023-01-03 16:05:34 +0100

    Calculator finish

--- index.js
+++ index.js
@@ -1,68 +1,100 @@
- var num1 = 0;
- var num2 = 0;
- var typeCalculation;
+var num1 = 0;
+var num2 = 0;
+var typeCalculation;
 
+function giveNumber(number) {
+  if (num1 == 0 && num1 !== "0.") {
+    num1 = number;
+  } else {
+    num1 += number;
+  }
+  toReset();
+}
 
-function giveNumber(number){
- if(num1==0 && num1 !== '0.'){
-     num1 = number;
- }else{
-     num1 += number;
- }
- toReset();
+function giveComma() {
+  if (num1 == 0) {
+    num1 = "0.";
+  } else if (num1.indexOf(".") == -1) {
+    num1 += ".";
+  }
+  toReset();
+}
+
+function giveC() {
+  num1 = 0;
+  num2 = 0;
+  toReset();
+}
+
+function calculate(valor) {
+  if (num1 == 0) {
+    num1 = parseFloat(document.getElementById("val_number").value);
+  }
+  num2 = parseFloat(num1);
+  num1 = 0;
+  typeCalculation = valor;
 }
 
 
-function giveComma(){
- if(num1 == 0) {
-     num1 = '0.';
- } else if(num1.indexOf('.') == -1) {
-     num1 += '.';
- }
- toReset();
+function sum (num1, num2)
+{
+    var resultado;
+    resultado = num1 + num2;
+    return resultado;
 }
 
-
-function giveC(){
- num1 = 0;
- num2 = 0;
- toReset();
+function subtrac (num1, num2)
+{
+    var resultado;
+    resultado = num2 - num1;
+    return resultado;
 }
 
-
-function calculate(valor){
- if (num1 == 0){
-     num1 = parseFloat(document.getElementById("val_number").value);
- }
- num2 = parseFloat(num1);
- num1= 0;
- typeCalculation = valor;
+function multiply (num1, num2)
+{
+    var resultado;
+    resultado = num1 * num2;
+    return resultado;
 }
 
-function equal(){
- num1 = parseFloat(num1);
- switch (typeCalculation){
-     case 1:
-         num1 += num2;
-     break;
-     case 2:
-         num1 = num2 - num1;
-     break;
-     case 3:
-         num1 *= num2;
-     break;
-     case 4:
-         num1 = num2 / num1;
-     break;
-     case 5:
-         num1 = Math.pow(num2, num1);
-     break;
- }
- toReset();
- num2 = parseFloat(num1);
- num1 = 0;
+function div (num1, num2)
+{
+    var resultado;
+    resultado = num2 / num1;
+    return resultado;
 }
 
-function toReset(){
- document.getElementById("val_number").value = num1;
+function powe (num1, num2)
+{
+    var resultado;
+    resultado =  Math.pow(num2, num1);
+    return resultado;
+}
+
+function equal() {
+  num1 = parseFloat(num1);
+  switch (typeCalculation) {
+    case 1:
+      num1 = sum(num1,num2);
+      break;
+    case 2:
+      num1 = subtrac (num1, num2);
+      break;
+    case 3:
+      num1 = multiply (num1, num2);
+      break;
+    case 4:
+      num1 = div (num1, num2);
+      break;
+    case 5:
+      num1 = powe (num1, num2);
+      break;
+  }
+  toReset();
+  num2 = parseFloat(num1);
+  num1 = 0;
+}
+
+function toReset() {
+  document.getElementById("val_number").value = num1;
 }
